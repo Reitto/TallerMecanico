2d1406bf87674d932c4c0fc74b2d5bdae91f9506
Reight <Adrianvisiedo2002@gmail.com>
2024-02-13 21:38:30 +0100

    Revisión y Clientes finalizados

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Revision.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Revision.java
@@ -3,6 +3,7 @@ package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
 
 import javax.naming.OperationNotSupportedException;
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
@@ -11,7 +12,7 @@ public class Revision {
     private Vehiculo vehiculo;
     private static final float PRECIO_HORA = 30;
     private static final float PRECIO_DIA = 10;
-    private static final float PRECIO_MATERIAL = 0F;
+    private static final float PRECIO_MATERIAL = 1.5F;
     static final DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private LocalDate fechaInicio;
     private LocalDate fechaFin;
@@ -25,8 +26,8 @@ public class Revision {
     }
 
     public Revision(Revision revision) {
-        Objects.requireNonNull(revision,"La revisión no puede ser nula.");
-        cliente = revision.cliente;
+        Objects.requireNonNull(revision, "La revisión no puede ser nula.");
+        cliente = new Cliente(revision.cliente);
         vehiculo = revision.vehiculo;
         fechaInicio = revision.fechaInicio;
         fechaFin = revision.fechaFin;
@@ -58,7 +59,7 @@ public class Revision {
 
     public void setFechaInicio(LocalDate fechaInicio) {
         Objects.requireNonNull(fechaInicio, "La fecha de inicio no puede ser nula.");
-        if (fechaInicio.isAfter(LocalDate.now())){
+        if (fechaInicio.isAfter(LocalDate.now())) {
             throw new IllegalArgumentException("La fecha de inicio no puede ser futura.");
         }
         this.fechaInicio = fechaInicio;
@@ -70,6 +71,12 @@ public class Revision {
 
     public void setFechaFin(LocalDate fechaFin) {
         Objects.requireNonNull(fechaFin, "La fecha de fin no puede ser nula.");
+        if (fechaFin.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException("La fecha de fin no puede ser futura.");
+        }
+        if (fechaFin.isBefore(fechaInicio)) {
+            throw new IllegalArgumentException("La fecha de fin no puede ser anterior a la fecha de inicio.");
+        }
         this.fechaFin = fechaFin;
     }
 
@@ -78,10 +85,10 @@ public class Revision {
     }
 
     public void anadirHoras(int horas) throws OperationNotSupportedException {
-        if (estaCerrada()){
+        if (estaCerrada()) {
             throw new OperationNotSupportedException("No se puede añadir horas, ya que la revisión está cerrada.");
         }
-        if (horas<=0){
+        if (horas <= 0) {
             throw new IllegalArgumentException("Las horas a añadir deben ser mayores que cero.");
         }
         this.horas = this.horas + horas;
@@ -92,10 +99,10 @@ public class Revision {
     }
 
     public void anadirPrecioMaterial(float precioMaterial) throws OperationNotSupportedException {
-        if (precioMaterial<=0){
+        if (precioMaterial <= 0) {
             throw new IllegalArgumentException("El precio del material a añadir debe ser mayor que cero.");
         }
-        if (estaCerrada()){
+        if (estaCerrada()) {
             throw new OperationNotSupportedException("No se puede añadir precio del material, ya que la revisión está cerrada.");
         }
         this.precioMaterial = this.precioMaterial + precioMaterial;
@@ -106,25 +113,23 @@ public class Revision {
     }
 
     public void cerrar(LocalDate fechaFin) throws OperationNotSupportedException {
-        Objects.requireNonNull(fechaFin,"La fecha de fin no puede ser nula.");
-        if (fechaFin.isBefore(fechaInicio)){
-            throw new IllegalArgumentException("La fecha de fin no puede ser anterior a la fecha de inicio.");
-        }
-        if (estaCerrada()){
+        Objects.requireNonNull(fechaFin, "La fecha de fin no puede ser nula.");
+        if (estaCerrada()) {
             throw new OperationNotSupportedException("La revisión ya está cerrada.");
-        }
-        if (fechaFin.isAfter(LocalDate.now())){
-            throw new IllegalArgumentException("La fecha de fin no puede ser futura.");
         }
         setFechaFin(fechaFin);
     }
 
     public float getPrecio() {
-        return 1.5F;
+        float precio = 0;
+        if (estaCerrada()) {
+            precio = PRECIO_MATERIAL * precioMaterial + ((horas * PRECIO_HORA) + (getDias() * PRECIO_DIA));
+        }
+        return precio;
     }
 
     private float getDias() {
-        return (float) horas /24;
+        return Period.between(fechaInicio, fechaFin).getDays();
     }
 
     @Override
@@ -141,6 +146,13 @@ public class Revision {
 
     @Override
     public String toString() {
-        return String.format("%s - %s: (%s - %s), 0 horas, 0,00 € en material, 10,00 € total", cliente, vehiculo, fechaInicio.format(FORMATO_FECHA), fechaFin.format(FORMATO_FECHA));
+        String cadena = null;
+        if (!estaCerrada()){
+            cadena = String.format("%s - %s: (%s - ), %d horas, %.2f € en material", cliente, vehiculo, fechaInicio.format(FORMATO_FECHA), horas, getPrecioMaterial());
+        }
+        if (estaCerrada()){
+            cadena = String.format("%s - %s: (%s - %s), %d horas, %.2f € en material, %.2f € total", cliente, vehiculo, fechaInicio.format(FORMATO_FECHA), fechaFin.format(FORMATO_FECHA), horas, getPrecioMaterial(), getPrecio());
+        }
+        return cadena;
     }
 }
--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Clientes.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Clientes.java
@@ -1,4 +1,63 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
 
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Clientes {
+    private final List<Cliente> coleccionClientes;
+
+    public Clientes() {
+        coleccionClientes = new ArrayList<>();
+    }
+
+    public List<Cliente> get() {
+        return coleccionClientes;
+    }
+
+    public void insertar(Cliente cliente) throws OperationNotSupportedException {
+        Objects.requireNonNull(cliente, "No se puede insertar un cliente nulo.");
+        if (coleccionClientes.contains(cliente)) {
+            throw new OperationNotSupportedException("Ya existe un cliente con ese DNI.");
+        }
+        coleccionClientes.add(cliente);
+    }
+
+    public boolean modificar(Cliente cliente, String nombre, String telefono) throws OperationNotSupportedException {
+        Objects.requireNonNull(cliente, "No se puede modificar un cliente nulo.");
+        boolean modificador = false;
+        if (!coleccionClientes.contains(cliente)) {
+            throw new OperationNotSupportedException("No existe ningún cliente con ese DNI.");
+        }
+        if (nombre != null) {
+            cliente.setNombre(nombre);
+            modificador = true;
+        }
+        if (telefono != null) {
+            cliente.setTelefono(telefono);
+            modificador = true;
+
+        }
+        return modificador;
+    }
+
+    public Cliente buscar(Cliente cliente) {
+        Objects.requireNonNull(cliente, "No se puede buscar un cliente nulo.");
+        if (!coleccionClientes.contains(cliente)) {
+            cliente = null;
+        }
+        return cliente;
+    }
+
+    public void borrar(Cliente cliente) throws OperationNotSupportedException {
+        Objects.requireNonNull(cliente, "No se puede borrar un cliente nulo.");
+        if (coleccionClientes.contains(cliente)) {
+            coleccionClientes.remove(cliente);
+        } else {
+            throw new OperationNotSupportedException("No existe ningún cliente con ese DNI.");
+        }
+    }
 }
